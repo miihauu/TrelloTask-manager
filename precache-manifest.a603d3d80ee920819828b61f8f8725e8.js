392d304f2e587bcb28b157320142e45f4187106a
unknown <miihauu@gmail.com>
2019-08-28 19:38:51 +0200

    Updates

---
+++ precache-manifest.a603d3d80ee920819828b61f8f8725e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e856bda7c988e12fea39ee78ee6842",
+    "url": "/TrelloTask-manager/index.html"
+  },
+  {
+    "revision": "d72e41421bc32ba39f34",
+    "url": "/TrelloTask-manager/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "42fde851a6f7478ec6bb",
+    "url": "/TrelloTask-manager/static/css/main.f506b25e.chunk.css"
+  },
+  {
+    "revision": "d72e41421bc32ba39f34",
+    "url": "/TrelloTask-manager/static/js/2.f44f6621.chunk.js"
+  },
+  {
+    "revision": "42fde851a6f7478ec6bb",
+    "url": "/TrelloTask-manager/static/js/main.f47c0395.chunk.js"
+  },
+  {
+    "revision": "a3af3f18dcb56675ec73",
+    "url": "/TrelloTask-manager/static/js/runtime~main.3385ca63.js"
+  },
+  {
+    "revision": "5d197581b46cf969f597955aafb365e1",
+    "url": "/TrelloTask-manager/static/media/dots.5d197581.svg"
+  }
+]);
